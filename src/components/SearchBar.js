3a4df7c567eeb370d71353c6eb08c45394592dd3
RL2020 <loc.fitness1@gmail.com>
2022-10-18 13:11:30 -0400

    some prevent default on search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,37 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
 
 class SearchBar extends React.Component {
+    state = { term: ''};
+
+    //event callback handler
+    onInputChange = event => {
+        this.setState({ term: event.target.value})
+    };
+
+    onFormSubmit = event => {
+        event.preventDefault();
+        // Todo: make sure we call callback from parent component
+    };
+
     render() {
-        return <div>Search Bar</div>
+        return (
+            <Container >  
+            <Form onSubmit={this.onFormSubmit} className="d-md-flex">
+            <Form.Control
+              type="search"
+              value={this.state.term}
+              onChange={this.onInputChange}
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+          </Container>
+        );
     }
 }
 
